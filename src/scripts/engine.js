75137b9950ddf44d6f2e272a10c63a9234e1fba8
Marcos Ariel Rodrigues de Jesus <marcosariel.rj@hotmail.com>
2024-02-22 11:39:41 -0300

    Adição

---
+++ src/scripts/engine.js
@@ -0,0 +1,15 @@
+const state = {
+    score:{
+        playerScore: 0,
+        computerScore: 0,
+        pontuacaocaixa: document.getElementById("pontos"),
+    },
+};
+
+
+function init(){
+
+
+}
+
+init();
